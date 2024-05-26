7019a8bdbdb3b9866d65972e4478c659d5ed40b8
Muhammad Daniyal <mdaniyal5454@gmail.com>
2024-05-26 16:57:54 +0500

    Comments added

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ let amount = document.querySelector(".input-amount input");
 
 // Loop to access the select elements and adding options to them according to the countries list file
 for(let select of dropdownMenu) {
+    // Loop to get currency code from countries list (key)
     for(currencyCode in countriesList) {
-        // Creating option elements in select to add currencies code
+        // Creating option elements in select to add currency codes
         let options = document.createElement("option");
         options.innerText = currencyCode;
         options.value = currencyCode;
@@ -26,15 +27,22 @@ for(let select of dropdownMenu) {
         select.append(options);
     }
 
+    // Adding event listener to select for tracking the change in options
     select.addEventListener("change", (evt) => {
         flagUpdate(evt.target);
     });
 }
 
+// Function to update the flag
 const flagUpdate = (element) => {
+    // Accessing flag image from parent element of select element
     let flagImage = element.parentElement.querySelector("img");
+    // Getting the value of currency code from select element
     let currencyCode = element.value;
+    // Getting the country code from countries list
     let countryCode = countriesList[currencyCode];
+    // Generating a new link using country code for different currencies
     let newSource = `https://flagsapi.com/${countryCode}/shiny/64.png`;
+    // Updating the flag image according to selected currency
     flagImage.src = newSource;
 }
