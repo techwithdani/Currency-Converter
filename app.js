ec13d292083744ca8a5437bfcef4cfcede9cf3e2
Muhammad Daniyal <mdaniyal5454@gmail.com>
2024-05-26 14:00:29 +0500

    HTML and JavaScript file updated

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-let baseUrl = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json"
+// Base URL for sending request to API
+let baseUrl = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 
-let dropdownMenu = document.querySelectorAll(".dropdown-menu select")
+// Selecting elements from HTML file with DOM
+let dropdownMenu = document.querySelectorAll(".dropdown-menu select");
 
+// Loop to access the select elements and adding options to them according to the countries list file
+for(let select of dropdownMenu) {
+    for(currencyCode in countriesList) {
+        // Creating option elements in select to add currencies code
+        let options = document.createElement("option");
+        options.innerText = currencyCode;
+        options.value = currencyCode;
+
+        // Condition to keep default selection of USD for "from" and PKR for "to"
+        if (options.value === "USD" && select.name === "from") {
+            options.selected = "selected";
+        }
+
+        if (options.value === "PKR" && select.name === "to") {
+            options.selected = "selected";
+        }
+
+        // Appending all the options created in select element
+        select.append(options);
+    }
+}
